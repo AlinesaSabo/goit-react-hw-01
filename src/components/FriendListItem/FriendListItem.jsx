603f885cd7dc3a9552962d4232ef3a7ebb44c958
Alinesa <alinesa322@gmail.com>
2024-08-31 13:06:00 +0300

    add fix frnd-listitem

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,15 +1,15 @@
-import s from "./FriendList.module.css";
+import s from "./FriendListItem.module.css";
 import clsx from "clsx";
 
 const FriendListItem = ({ friend }) => {
   return (
-    <div>
+    <>
       <img src={friend.avatar} width="48" />
       <p>{friend.name}</p>
       <p className={clsx(friend.isOnline ? s.green : s.red)}>
         {friend.isOnline ? "Online" : "Offline"}
       </p>
-    </div>
+    </>
   );
 };
 
